a5e2bc3d697236ef0b31ba8d02013457843f7c2e
Al Mahamud Murad <muradhasan.7845@gmail.com>
2022-07-23 13:49:32 +0600

    code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,28 @@ function App() {
   const paths = ["service", "comments", "user", "product"];
   const [serviceData, setServiceData] = useState([]);
   const [userData, setUserData] = useState([]);
+  const [productData, setProductData] = useState([]);
 
-    function getDataByPath(path) {
-      getApiData(path).then((res) => {
-        switch (path) {
-          case "service":
-            setServiceData(res.data);
-            break;
-          case "user":
-            setUserData(res.data);
-            break;
-          default:
-            break;
-        }
-      });
-    }
+  function getDataByPath(path) {
+    getApiData(path).then((res) => {
+      switch (path) {
+        case "service":
+          setServiceData(res.data);
+          break;
+        case "user":
+          setUserData(res.data);
+          break;
+        case "product":
+          setProductData(res.data);
+          break;
+        default:
+          break;
+      }
+    });
+  }
 
-  console.log(userData);
+  const highPrice = productData.filter((item) => item.price > 100);
+  console.log(highPrice);
 
   return (
     <div className="App">
@@ -44,6 +49,16 @@ function App() {
         return (
           <div key={index}>
             <h2>{data.username}</h2>
+          </div>
+        );
+      })}
+      <button className="button" onClick={() => getDataByPath(paths[3])}>
+        Get Product Data
+      </button>
+      {productData.map((data, index) => {
+        return (
+          <div key={index}>
+            <h2>{data.title}</h2>
           </div>
         );
       })}
