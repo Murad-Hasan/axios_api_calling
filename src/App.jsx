8652fddbe2084b778f81fb25d1bfa461126031c5
Al Mahamud Murad <muradhasan.7845@gmail.com>
2022-07-23 13:17:33 +0600

    code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,49 @@ import { getApiData } from "./network/lib/apiData";
 
 function App() {
   const paths = ["service", "comments", "user", "product"];
-  useEffect(() => {
-    paths.forEach((path) => {
+  const [serviceData, setServiceData] = useState([]);
+  const [userData, setUserData] = useState([]);
+
+    function getDataByPath(path) {
       getApiData(path).then((res) => {
-        console.log(`${path}` , res.data);
+        switch (path) {
+          case "service":
+            setServiceData(res.data);
+            break;
+          case "user":
+            setUserData(res.data);
+            break;
+          default:
+            break;
+        }
       });
-    });
-  }, []);
+    }
+
+  console.log(userData);
+
   return (
     <div className="App">
       <h1>API Calling </h1>
-      <button className="button">Get Service Data</button>
+      <button className="button" onClick={() => getDataByPath(paths[0])}>
+        Get Service Data
+      </button>
+      {serviceData.map((data, index) => {
+        return (
+          <div key={index}>
+            <h2>{data.title}</h2>
+          </div>
+        );
+      })}
+      <button className="button" onClick={() => getDataByPath(paths[2])}>
+        Get User Data
+      </button>
+      {userData.map((data, index) => {
+        return (
+          <div key={index}>
+            <h2>{data.username}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
